594db629ce7d725959c2c0ed247c95c210e7e286
Alejandro Garcia <106962476+ajurgar@users.noreply.github.com>
2022-10-12 10:59:51 +0100

    added Alejandro as customer and a booking

--- backend/src/main/java/com/example/backend/components/DataLoader.java
+++ backend/src/main/java/com/example/backend/components/DataLoader.java
@@ -11,6 +11,7 @@ import javax.annotation.PostConstruct;
 import java.awt.*;
 import java.awt.print.Book;
 import java.time.LocalDate;
+import java.time.LocalTime;
 import java.util.ArrayList;
 
 @Component
@@ -26,17 +27,35 @@ public class DataLoader {
 
     @PostConstruct
     public void setup() {
-        Customer customer = new Customer();
-        customer.setBookings(new ArrayList<>());
-        customer.setName("Juan");
+        Customer customer1 = new Customer();
+        customer1.setBookings(new ArrayList<>());
+        customer1.setName("Juan");
 
-        Booking booking = new Booking();
-        booking.setTableNumber(1);
-        booking.setCustomer(customer);
-        booking.setDate(LocalDate.of(2022,1,1));
+        Booking booking1 = new Booking();
+        booking1.setTableNumber(1);
+        booking1.setCustomer(customer1);
+        booking1.setDate(LocalDate.of(2022,1,1));
+        booking1.setTime(LocalTime.of(12, 00));
 
-        customerRepository.save(customer);
-        bookingRepository.save(booking);
+        customerRepository.save(customer1);
+        bookingRepository.save(booking1);
+
+        Customer customer2 = new Customer();
+        customer2.setBookings(new ArrayList<>());
+        customer2.setName("Alejandro");
+
+        Booking booking2 = new Booking();
+        booking2.setTableNumber(2);
+        booking2.setCustomer(customer2);
+        booking2.setDate(LocalDate.of(2022,5,15));
+        booking2.setTime(LocalTime.of(13, 00));
+
+        customerRepository.save(customer2);
+        bookingRepository.save(booking2);
+
+
     }
+
+
 
 }
