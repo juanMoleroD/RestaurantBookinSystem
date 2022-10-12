f4e43cf494dcb263fef8640c5ce7db789b5b87b8
Alejandro Garcia <106962476+ajurgar@users.noreply.github.com>
2022-10-12 13:54:26 +0100

    added booking props

--- frontend/src/components/Booking.js
+++ frontend/src/components/Booking.js
@@ -1,3 +1,20 @@
 import React from "react";
 
 
+const Booking = ({booking}) => {
+
+
+
+        return(
+
+            <>
+               <p>{booking.tableNumber}</p> 
+               <p>{booking.date}</p> 
+               <p>{booking.time}</p> 
+               <p>{booking.customer.name}</p>
+            </>
+        )
+
+}
+
+export default Booking;
--- frontend/src/components/BookingList.js
+++ frontend/src/components/BookingList.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Booking from "./Booking";
 
 
 const BookingList = ({bookings}) => {
